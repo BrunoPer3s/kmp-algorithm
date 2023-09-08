9e953159c4035f47cd203f071b6b0c081cf9e1ac
BrunoPer3s <brunoperes7@hotmail.com>
2023-09-08 18:26:13 -0300

    fix: logic and sintax

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #define m 5
 
-void lps(int aux[M], char P[M]) {
+void lps(int aux[m], char P[m]) {
   int j = 0;
   aux[0] = j;
   for(int i = 1; i < m; i++) {
-    while(j > 0 && P[j] == P[i]) {
+    while(j > 0 && P[j] != P[i]) {
       j = aux[j-1];
     }
     if(P[j] == P[i]) {
